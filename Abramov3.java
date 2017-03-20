b3f44141524c7b70855690716e90be7c6f299a75
tahirmm <munarbekovt@gmail.com>
2017-03-20 12:22:22 +0600

    Add files via upload

---
+++ Abramov3.java
@@ -0,0 +1,25 @@
+package Abramov1;
+
+
+
+
+
+public class Abramov3 {
+
+    
+    public static void main(String[] args) {
+        int a=3,c,V;
+       Math.pow(a,3);//
+       c=6*(a*a);
+    }
+
+    public static class Abramov2 {
+
+        public static void main(String[] args) {
+            int a = 1;
+            int b = 1;
+            System.out.println((Math.abs(a) - Math.abs(b)) / (1 + Math.abs(a * b)));
+        }
+    }
+    
+}
---
+++ Abramov6.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Abramov1;
+
+/**
+ *
+ * @author Tahir
+ */
+public class Abramov6 {
+    public static void main(String[] args) {
+        int a=3,b=3;
+            System.out.println(Math.sqrt(Math.pow(a, 2)+Math.pow(b,2)));
+            System.out.println(1/2*a*b);
+            
+    }
+}
